0ea7ac626c471473874cd7f0167a74066cf8455d
DTH235623DuongThanhDat <ddat2376@gmail.com>
2025-08-29 14:53:18 +0700

    project

--- Vidu7.py
+++ Vidu7.py
@@ -1,8 +1,8 @@
-#module1.py
+# module1.py
 question = 'What is the meaning of Life, the Universe, and Everything?'
 answer = 42
 
-#module2.py
+# module2.py
 question = 'What is your quest?'
 answer = 'To seek the holy grail.'
 
