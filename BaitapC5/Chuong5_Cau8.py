7bf005b84a7ecbfc8f639dc11e4729119da67ab8
DTH235623DuongThanhDat <ddat2376@gmail.com>
2025-10-21 15:17:20 +0700

    BTC6

--- BaitapC5/Chuong5_Cau8.py
+++ BaitapC5/Chuong5_Cau8.py
@@ -6,4 +6,4 @@ def LayTenFileKhongDuoi(path):
     return os.path.splitext(os.path.basename(path))[0]
 duong_dan = input("Nhập vào đường dẫn file nhạc: ")
 print("Tên file có đuôi:", LayTenFileDuoi(duong_dan))       # muabui.mp3
-print("Tên file không đuôi:", LayTenFileKhongDuoi(duong_dan))  # muabui
+print("Tên file không đuôi:", LayTenFileKhongDuoi(duong_dan))  
